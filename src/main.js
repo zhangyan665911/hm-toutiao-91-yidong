4de9928052ef590e7a51f4902109cb0d77f4179b
张燕 <584781562@qq.com>
2020-01-11 15:10:43 +0800

    提交了layout布局

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'amfe-flexible' // 引入自动适配
+import '@/styles/index.less'
 Vue.use(Vant)// 一旦完成注冊 在任意地方都可以用
 
 Vue.config.productionTip = false
