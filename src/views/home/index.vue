b33ac4b4d3b01fad50a3abc5e0a92f3b05f74bc9
张燕 <584781562@qq.com>
2020-01-11 10:08:25 +0800

    修改了目录结构

---
+++ src/views/home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>主页</div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/user/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>个人</div>
+</template>
+
+<script>
+export default {
+  name: 'user'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
