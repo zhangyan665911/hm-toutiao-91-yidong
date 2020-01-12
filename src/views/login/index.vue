935fc1d1c5129392a3b0fdb29b72298350d7f011
张燕 <584781562@qq.com>
2020-01-12 11:57:12 +0800

    修改主题颜色

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()" ></van-nav-bar>
+    <van-cell-group>
+      <van-field label="手机号" placeholder="请输入手机号" required></van-field>
+      <van-field label="验证码" placeholder="请输入验证码" required>
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="btn-box">
+      <van-button type="info" block round size="small">登录</van-button>
+    </div>
+
   </div>
 </template>
 
@@ -11,4 +21,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.btn-box{
+  padding:20px;
+}
 </style>
