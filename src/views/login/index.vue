a4684af6c695a1ad3a1bb776c3e9a1e7c669afa2
张燕 <584781562@qq.com>
2020-01-12 12:18:36 +0800

    登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,13 +2,13 @@
   <div class="container">
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()" ></van-nav-bar>
     <van-cell-group>
-      <van-field label="手机号" placeholder="请输入手机号" required></van-field>
-      <van-field label="验证码" placeholder="请输入验证码" required>
+      <van-field v-model="loginForm.mobile" label="手机号" @blur="checkMobile" :error-message="errorMsg.mobile" placeholder="请输入手机号" required></van-field>
+      <van-field v-model="loginForm.code" label="验证码" placeholder="请输入验证码" @blur="checkCode" required :error-message="errorMsg.code">
         <van-button slot="button" size="small" type="primary">发送验证码</van-button>
       </van-field>
     </van-cell-group>
     <div class="btn-box">
-      <van-button type="info" block round size="small">登录</van-button>
+      <van-button type="info" block round size="small" @click="login">登录</van-button>
     </div>
 
   </div>
@@ -16,7 +16,52 @@
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      // 专门提示错误信息的
+      errorMsg: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    checkMobile () {
+      // 判断为空
+      if (!this.loginForm.mobile) {
+        this.errorMsg.mobile = '手机号不能为空'
+        return false
+      } else if (!/^1[3-9]\d{9}$/.test(this.loginForm.mobile)) {
+        this.errorMsg.mobile = '手机号格式不正确'
+        return false
+      }
+      this.errorMsg.mobile = ''
+      return true
+    },
+    checkCode () {
+      if (!this.loginForm.code) {
+        this.errorMsg.code = '验证码不能为空'
+        return false
+      } else if (!/^\d{6}$/.test(this.loginForm.code)) {
+        this.errorMsg.code = '验证码必须为6位'
+        return false
+      }
+      this.errorMsg.code = ''
+      return true
+    },
+    login () {
+      if (this.checkMobile() && this.checkCode()) {
+        // 发送请求
+        // 提示消息 表示登录成功
+        console.log('校验通过')
+      }
+    }
+  }
 }
 </script>
 
