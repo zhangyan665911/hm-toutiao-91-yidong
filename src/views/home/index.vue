47661e936bda336c925d632321d9c9fda87182d8
张燕 <584781562@qq.com>
2020-01-12 15:42:43 +0800

    首页列表的布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,87 @@
 <template>
-  <div class="container">主页</div>
+  <div class="container">
+    <van-tabs swipeable v-model="activeIndex">
+      <van-tab :title="'标签'+item" v-for="item in 10" :key="item" >
+        <!-- 这个div设置了滚动条 目的是给后面做阅读记忆留下伏笔 -->
+        <!-- 阅读记忆=>看文章看到一半滑到一半回来还是原来看的位置 -->
+        <div class="scroll-wrapper">
+          <van-cell-group>
+            <van-cell :title="item"  v-for="obj in 20" :key="obj"/>
+          </van-cell-group>
+        </div>
+      </van-tab>
+    </van-tabs>
+    <span class="bar_btn">
+      <van-icon name="wap-nav"></van-icon>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'home',
+  data () {
+    return {
+      activeIndex: 0
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content{
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane{
+    height: 100%;
+    .scroll-wrapper{
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
