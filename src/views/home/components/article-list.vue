537f56a23c8e5f0dda64876d48a56fd25340b355
张燕 <584781562@qq.com>
2020-01-14 09:31:31 +0800

    提交了article-list假数据上拉加载

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,3 +1,4 @@
+/* eslint-disable space-before-blocks */
 <template>
   <div class="scroll-wrapper">
         <van-list
@@ -6,11 +7,11 @@
             finished-text="没有更多了"
             @load="onLoad"
             >
-            <!-- <van-cell
-                v-for="item in list"
-                :key="item"
-                :title="item"
-            /> -->
+            <van-cell
+                v-for="article in articles"
+                :key="article"
+                :title="article"
+            />
         </van-list>
     </div>
 </template>
@@ -21,13 +22,25 @@ export default {
   data () {
     return {
       upLoading: false, // 是否开启上拉加载
-      finished: false// 是否完成全部加载
+      finished: false, // 是否完成全部加载
+      articles: []// 定义一个数组来接收上拉加载的数据
     }
   },
   methods: {
+    // van-list组件当你的组件内容距离底部超过一定长度的时候就会再调用onload
     onLoad () {
-      // 上拉加载方法
-      console.log('开始加载数据')
+      setTimeout(() => {
+        // 给数据设置一个上限，不能超过50条，如果
+        if (this.articles.length < 50) {
+          // 上拉加载方法
+          let arr = Array.from(Array(10), (value, index) => this.articles.length + index + 1)
+          this.articles.push(...arr)
+          // 关闭状态
+          this.upLoading = false
+        } else {
+          this.finished = true
+        }
+      }, 1000)
     }
   }
 }
