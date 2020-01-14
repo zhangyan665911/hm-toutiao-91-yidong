bbd74863b31356ffc88f6e101ceae86964744f32
张燕 <584781562@qq.com>
2020-01-14 14:50:18 +0800

    提交了图片懒加载

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import App from './App.vue'
 import router from '@/permission'
 // import '@/permission'
 import store from './store'
-import Vant from 'vant'
+import Vant, { Lazyload } from 'vant' // 引入一个lazyload对象
 import 'vant/lib/index.less'
 import plugins from '@/utils/plugins'
 import 'amfe-flexible' // 引入自动适配
 import '@/styles/index.less' // 必须放在vant样式下面 要不覆盖不了
 Vue.use(Vant)// 一旦完成注冊 在任意地方都可以用
 Vue.use(plugins)
+Vue.use(Lazyload)
 
 Vue.config.productionTip = false
 
